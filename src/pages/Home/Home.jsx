6e867112773f3ce1665419963204c0ffdc05b3dd
gr8kev <82469036+gr8kev@users.noreply.github.com>
2024-11-01 20:33:58 +0100

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HomeModule from "../../component/Home/HomeModule";
+import About from "../../component/Home/About";
 
 export const Home = () => {
   return (
     <div>
       <HomeModule />
+      <About />
     </div>
   );
 };
